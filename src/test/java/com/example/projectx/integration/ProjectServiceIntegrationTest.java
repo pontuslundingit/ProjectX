8c9eb755b0542c2380b2e9add33bbbea9472c9db
Pontus Lundin <pontus.lundin@yh.nackademin.se>
2024-10-02 18:33:55 +0200

    Here we go again 2...

--- src/test/java/com/example/projectx/integration/ProjectServiceIntegrationTest.java
+++ src/test/java/com/example/projectx/integration/ProjectServiceIntegrationTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -17,6 +18,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 public class ProjectServiceIntegrationTest {
 
